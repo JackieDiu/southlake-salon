1b28fa303e6c8fc00e07feb66b80c8882af8ba43
Jackie Diu <jackiediu@gmail.com>
2019-05-13 13:18:07 -0700

    removed sticky navbar

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+$(window).on("scroll", function() {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("white");
+  } else {
+    $("nav").removeClass("white");
+  }
+});
+
 $(function() {
   setTimeout(function() {
     $(".text-animation").removeClass("hidden");
